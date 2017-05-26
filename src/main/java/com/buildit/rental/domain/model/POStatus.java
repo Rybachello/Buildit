e0562ac4ecc17660409d6e3dfec50fac910fa923
rybachello <mykolarybachello@gmail.com>
2017-05-26 03:12:58 +0300

    + add new po statuses

--- src/main/java/com/buildit/rental/domain/model/POStatus.java
+++ src/main/java/com/buildit/rental/domain/model/POStatus.java
@@ -1,5 +1,12 @@
 package com.buildit.rental.domain.model;
 
 public enum POStatus {
-    PENDING, OPEN, REJECTED, CLOSED
+    PENDING,
+    REJECTED,
+    REJECTED_BY_CUSTOMER,
+    OPEN,
+    DISPATCHED,
+    RETURNED,
+    DELIVERED,
+    CLOSED,
 }
